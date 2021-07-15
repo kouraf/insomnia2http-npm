6f94ccdd3d7793f69e94579a0136e31a05bc5b87
Ahmed Farouk YEDDOU <yeddoufarouk@gmail.com>
2021-07-15 10:50:29 +0200

    ignore

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -5,8 +5,10 @@ function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === "Windows_NT" && arch === "x64") return "win64";
-  if (type === "Windows_NT") return "win32";
+  if (type === "Windows_NT" && arch === "x64") return "Windows_x86_64";
+
+  if (type === "Windows_NT") return "Windows_i386";
+
   if (type === "Linux" && arch === "x64") return "linux";
   if (type === "Darwin" && arch === "x64") return "macos";
 
@@ -15,7 +17,7 @@ function getPlatform() {
 
 function getBinary() {
   const platform = getPlatform();
-  const version = require("../package.json").version;
+  const version = "0.1.1"; // require("../package.json").version;
   const url = `https://github.com/kouraf/insomnia2http/releases/download/v${version}/insomnia2http_${version}_${platform}.tar.gz`;
   const name = "insomnia2http";
   return new Binary(name, url);
